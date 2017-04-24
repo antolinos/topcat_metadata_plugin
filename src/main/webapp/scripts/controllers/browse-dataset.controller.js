ce132ed8a083fc16b63f9b8e75f6444d744d162d
ale <ale@localhost.localdomain>
2017-04-24 09:50:19 +0200

    Techniques mix

--- src/main/webapp/scripts/controllers/browse-dataset.controller.js
+++ src/main/webapp/scripts/controllers/browse-dataset.controller.js
@@ -10,17 +10,31 @@
     	var that = this;
     	var investigationId = $state.params.investigationId;
     	var icat = tc.icat($state.params.facilityName);
+	
 
     	this.datasets = [];
 
     	icat.query([
     		"select dataset from Dataset dataset, dataset.investigation as investigation",
-    		"where investigation.id = ?", investigationId
+    		"where investigation.id = ? INCLUDE dataset.parameters.type, dataset.type", investigationId
     	]).then(function(datasets){
     		that.datasets = datasets;
+                /** Setting parameters at level of Dataset **/
+		
+		for (var i =0; i < datasets.length; i++){
+			console.log(datasets[i]);
+			for (var j =0; j < datasets[i].parameters.length; j++){
+			                console.log(datasets[i].parameters[j].type.name + " " + datasets[i].parameters[j].stringValue);
+					datasets[i][datasets[i].parameters[j].type.name] = datasets[i].parameters[j].stringValue;
+
+			}
+		}
+
     	});
 
     });
+
+ 
 
 })();
 
